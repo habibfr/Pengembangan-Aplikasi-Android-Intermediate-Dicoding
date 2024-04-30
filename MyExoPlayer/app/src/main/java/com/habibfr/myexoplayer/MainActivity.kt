e2a461fc054d97fd287188b1d57b9b80328010ac
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-04-30 21:23:57 +0700

    exo player with service

--- MyExoPlayer/app/src/main/java/com/habibfr/myexoplayer/MainActivity.kt
+++ MyExoPlayer/app/src/main/java/com/habibfr/myexoplayer/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.habibfr.myexoplayer
 
+import android.content.ComponentName
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.MoreExecutors
 import com.habibfr.myexoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +23,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val videoItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
-        val audioItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
+        val videoItem = MediaItem.Builder()
+            .setUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
+            .setMediaMetadata(
+                MediaMetadata.Builder()
+                    .setTitle("Bersama Dicoding, Kembangkan Dirimu Menjadi Talenta Digital Berstandar Global")
+                    .setArtist("Dicoding")
+                    .build()
+            ).build()
+
+        val audioItem = MediaItem.Builder()
+            .setUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
+            .setMediaMetadata(
+                MediaMetadata.Builder()
+                    .setTitle("Ukulele")
+                    .setArtist("Bensound")
+                    .build()
+            ).build()
 
         val player = ExoPlayer.Builder(this).build().also { exoPlayer ->
             exoPlayer.setMediaItem(videoItem)
@@ -31,6 +51,17 @@ class MainActivity : AppCompatActivity() {
         hideSystemUI()
     }
 
+
+    override fun onStart() {
+        super.onStart()
+        val sessionToken = SessionToken(this, ComponentName(this, PlaybackService::class.java))
+        val controllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
+        controllerFuture.addListener(
+            { binding.playerView.player = controllerFuture.get() },
+            MoreExecutors.directExecutor()
+        )
+    }
+
     private fun hideSystemUI() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         WindowInsetsControllerCompat(window, binding.playerView).let { controller ->
@@ -39,4 +70,5 @@ class MainActivity : AppCompatActivity() {
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
+
 }
