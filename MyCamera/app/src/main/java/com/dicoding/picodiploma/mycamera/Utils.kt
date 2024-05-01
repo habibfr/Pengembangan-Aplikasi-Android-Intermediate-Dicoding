1855375893e39e933d1c1a0a38626e6235713771
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-05-01 21:57:46 +0700

    camera x

--- MyCamera/app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
+++ MyCamera/app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
@@ -41,3 +41,7 @@ private fun getImageUriForPreQ(context: Context): Uri {
     )
 }
 
+fun createCustomTempFile(context: Context): File {
+    val filesDir = context.externalCacheDir
+    return File.createTempFile(timeStamp, ".jpg", filesDir)
+}
